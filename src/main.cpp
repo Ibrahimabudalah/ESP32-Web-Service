5ecbcc4abad39efc4904dac118be2ce044668c40
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-08-14 22:27:09 -0700

    initialized the serial connection and wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,21 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // initialize serial communication
+  Serial.begin(115200);
+  // initialize wifi by passing in the ssid and password -- .env file
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  // check if it's not connected
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println("Connecting to WiFi...");
+    delay(1000);
+  }
+
+  // print the successful connection and board IP address
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
